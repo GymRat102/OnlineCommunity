c5809a239bd250f662cb27c61e240b99d656dec3
Weijian_Duan <weijianduan0302@gmail.com>
2021-02-05 13:28:17 -0500

    (bug)开发社区搜索功能：   - 业务层增加搜索服务(ElasticsearchService)     > saveDiscussPost()保存帖子到ES服务器     > deleteDiscussPost()从ES服务器中删除帖子     > searchDiscussPost()从服务器中搜索帖子   - 发布事件     > 发布帖子时(DiscussPostController)，将帖子异步提交到ES服务器     > 增加评论时(CommentController)，将帖子异步提交到ES服务器     > 在消费组件(EventConsumer)中新增一个方法，消费帖子发布事件   - 显示结果     > 新增控制器SearchController处理搜索请求     > 配置前端页面index, search显示搜索结果
    存在bug:
      - 搜索结果页面不分页
      - 搜索出的帖子时间不正确
      - 搜索结果标题和内容显示不正确
    后续解决方向：
      - ElasticsearchService中的搜索方法返回值应该是ES的page类型，目前是List
      - 使用elasticsearchRestTemplate进行搜索如何返回page，以及SearchHit的导包问题
      - DiscussPost实体类中的ES日期注解

--- src/test/java/com/nowcoder/community/ElasticsearchTests.java
+++ src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -3,31 +3,22 @@ package com.nowcoder.community;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
 import com.nowcoder.community.entity.DiscussPost;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.SpringVersion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.*;
-import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
-import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
@@ -115,25 +106,75 @@ public class ElasticsearchTests {
         //elasticsearchTemplate.queryForPage()
         //elasticsearchRestTemplate.queryForPage(searchQuery, DiscussPost.class, new SearchRe)
 
-        SearchHits<DiscussPost> search = elasticsearchRestTemplate.search(searchQuery, DiscussPost.class);
-        // 得到查询结果返回的内容
-        List<SearchHit<DiscussPost>> searchHits = search.getSearchHits();
-        // 设置一个需要返回的实体类集合
-        List<DiscussPost> discussPosts = new ArrayList<>();
-        // 遍历返回的内容进行处理
-        for(SearchHit<DiscussPost> searchHit : searchHits){
-            // 高亮的内容
-            Map<String, List<String>> highLightFields = searchHit.getHighlightFields();
-            // 将高亮的内容填充到content中
-            searchHit.getContent().setTitle(highLightFields.get("title") == null ? searchHit.getContent().getTitle() : highLightFields.get("title").get(0));
-            searchHit.getContent().setTitle(highLightFields.get("content") == null ? searchHit.getContent().getContent() : highLightFields.get("content").get(0));
-            // 放到实体类中
-            discussPosts.add(searchHit.getContent());
+        // SearchHits搜索命中数据
+        SearchHits<DiscussPost> hits = elasticsearchRestTemplate.search(searchQuery, DiscussPost.class);
+        // Wrap the returned SearchPage<T> object in a SearchPage<T>
+        SearchPage<DiscussPost> searchPage = SearchHitSupport.searchPageFor(hits, searchQuery.getPageable());
+        System.out.println(searchPage.getTotalElements());
+        System.out.println(searchPage.getTotalPages());
+        System.out.println(searchPage.getNumber());
+        System.out.println(searchPage.getSize());
+        for (SearchHit<DiscussPost> post : searchPage) {
+            System.out.println(post);
         }
-        System.out.println(discussPosts.size());
-        for(DiscussPost discussPost : discussPosts){
-            System.out.println(discussPost);
-        }
+
+
+
+
+
+
+
+
+
+        // List<DiscussPost> list = new ArrayList<>();
+        // for(SearchHit<DiscussPost> hit : hits){ // hit是JSON格式的数据
+        //     DiscussPost post = new DiscussPost();
+        //     hit.
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Wrap the returned SearchPage<T> object in a SearchPage<T>
+        // SearchPage<DiscussPost> searchPage = SearchHitSupport.searchPageFor(search, searchQuery.getPageable());
+        //
+        // System.out.println(searchPage.getTotalElements());
+        // System.out.println(searchPage.getTotalPages());
+        // System.out.println(searchPage.getNumber());
+        // System.out.println(searchPage.getSize());
+        // for(SearchHit<DiscussPost> post : searchPage){
+        //     System.out.println(post);
+        // }
+
+        // // 得到查询结果返回的内容
+        // List<SearchHit<DiscussPost>> searchHits = search.getSearchHits();
+        // // 设置一个需要返回的实体类集合
+        // List<DiscussPost> discussPosts = new ArrayList<>();
+        // // 遍历返回的内容进行处理
+        // for(SearchHit<DiscussPost> searchHit : searchHits){
+        //     // 高亮的内容
+        //     Map<String, List<String>> highLightFields = searchHit.getHighlightFields();
+        //     // 将高亮的内容填充到content中
+        //     searchHit.getContent().setTitle(highLightFields.get("title") == null ? searchHit.getContent().getTitle() : highLightFields.get("title").get(0));
+        //     searchHit.getContent().setTitle(highLightFields.get("content") == null ? searchHit.getContent().getContent() : highLightFields.get("content").get(0));
+        //     // 放到实体类中
+        //     discussPosts.add(searchHit.getContent());
+        // }
+        // System.out.println(discussPosts.size());
+        // for(DiscussPost discussPost : discussPosts){
+        //     System.out.println(discussPost);
+        // }
     }
 
 }
