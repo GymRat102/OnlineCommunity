bda24f102504d2f7e8350a04cd1867c05b29cd14
Weijian_Duan <weijianduan0302@gmail.com>
2021-02-05 17:30:55 -0500

    利用Spring Security进行权限控制   - 登陆检查     > 之前采用的拦截器实现登陆检查是简单的权限管理方案，现在将其废弃   - 授权配置     > 对当前系统内包含的所有的请求，分配访问权限（普通用户、版主、管理员）   - 认证方案     > 绕过Security认证流程，采用系统原来的认证方案   - CSRF配置     > Request中增加TOKEN信息防止CSRF的攻击

--- src/main/java/com/nowcoder/community/config/WebMVCConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMVCConfig.java
@@ -18,8 +18,8 @@ public class WebMVCConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
 
-    @Autowired
-    private LoginRequiredInterceptor loginRequiredInterceptor;
+    // @Autowired
+    // private LoginRequiredInterceptor loginRequiredInterceptor;
 
     @Autowired
     private MessageInterceptor messageInterceptor;
@@ -33,8 +33,8 @@ public class WebMVCConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginTicketInterceptor)
                 .excludePathPatterns("/css/*.css", "/js/*.js", "/img/*.png", "/img/*.jpg", "/img/*.jepg");
 
-        registry.addInterceptor(loginRequiredInterceptor)
-                .excludePathPatterns("/css/*.css", "/js/*.js", "/img/*.png", "/img/*.jpg", "/img/*.jepg");
+        // registry.addInterceptor(loginRequiredInterceptor)
+        //         .excludePathPatterns("/css/*.css", "/js/*.js", "/img/*.png", "/img/*.jpg", "/img/*.jepg");
 
         registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/css/*.css", "/js/*.js", "/img/*.png", "/img/*.jpg", "/img/*.jepg");
